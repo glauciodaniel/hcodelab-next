151f01bb37e3ec35ad878b14c48d306daa498713
Anthony Rafael <anthonyribeiro1910@gmail.com>
2020-05-29 02:08:37 -0300

    Página Editar Usuário

--- components/admin/Card.js
+++ components/admin/Card.js
@@ -5,11 +5,15 @@ export default function Card(props) {
     return (
         <div className={styles.card}>
 
-            {props.children}
+            <div className={styles['card-content']}>
+
+                {props.children}
+
+            </div>
 
             <div className={styles.actions}>
 
-                <button>{props.btnText}</button>
+                <button className="button">{props.btnText}</button>
 
             </div>
 
--- pages/admin/users/[id].js
+++ pages/admin/users/[id].js
@@ -23,23 +23,71 @@ export default function Users() {
 
             <Header />
 
-            <main id="edit-user">
+            <main id={styles['edit-user']}>
                 
                 <HeaderTitle text="Editar Usuário" /> {/* Esse código poderá ser adicionado depois, se desejar */}
 
                 <section className={styles.cards}>
 
-                    <Card user={userObj} btnText="Teste">
+                    <Card btnText="Salvar" className={styles.card}>
 
-                        <>
-                            <h2>{userObj['name']}</h2>
-                            
-                            <p>{userObj['email']}</p>
+                        <div className={styles.header}>
 
-                            <p>{userObj['date']}</p>
-                        </>
+                            <h2>Dados Pessoais</h2>
 
-                        <img src={"/images/" + userObj['photo']} className='user-photo' />
+                            <button className="button-reset">
+                                <svg id="Grupo_112" data-name="Grupo 112" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                                    <g id="more_vert-black-18dp" transform="translate(4.017 4.05)">
+                                        <path id="Caminho_236" data-name="Caminho 236" d="M0,0H24V24H0Z" fill="none"/>
+                                        <path id="Caminho_237" data-name="Caminho 237" d="M12,8a2,2,0,1,0-2-2A2.006,2.006,0,0,0,12,8Zm0,2a2,2,0,1,0,2,2A2.006,2.006,0,0,0,12,10Zm0,6a2,2,0,1,0,2,2A2.006,2.006,0,0,0,12,16Z" fill="#666"/>
+                                    </g>
+                                    <rect id="Retângulo_73" data-name="Retângulo 73" width="32" height="32" fill="none"/>
+                                </svg>
+                            </button>
+
+                        </div>
+                        
+                        <form className={`${styles['form-user-data']} ${styles.form}`}>
+
+                            <input type="text" placeholder="Nome Completo" />
+
+                            <input type="email" placeholder="E-mail" />
+
+                            <input type="date" placeholder="Data de Nascimento" />
+
+                        </form>
+
+                    </Card>
+
+                    <Card btnText="Alterar">
+
+                        <div className={styles.header}>
+                        
+                            <h2>Senha</h2>
+
+                        </div>
+                        
+                        <form className={styles.form}>
+
+                            <input type="password" placeholder="Senha Atual" />
+
+                            <input type="password" placeholder="Nova Senha" />
+
+                            <input type="password" placeholder="Confirme a Nova Senha" />
+
+                        </form>
+
+                    </Card>
+
+                    <Card btnText="Escolher Foto">
+
+                        <div className={styles.header}>
+
+                            <h2>Avatar</h2>
+
+                        </div>
+
+                        <img src="/images/user-photo.png" className={styles.avatar} />
 
                     </Card>
 
